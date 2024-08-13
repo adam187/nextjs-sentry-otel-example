f1752f1b9fcd80accca6b8912f12d8b59243dd74
Adam Misiorny <adam.misiorny@gmail.com>
2024-08-13 18:04:48 +0200

    chore: TECH try sentry example

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,15 +1,17 @@
-import * as Sentry from "@sentry/nextjs";
-import { registerOTel } from "@vercel/otel";
-
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
+import * as Sentry from "@sentry/node";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import {
+  SentrySpanProcessor,
+  SentryPropagator,
+  SentrySampler,
+} from "@sentry/opentelemetry";
 import type {
   ReadableSpan,
   Span,
   SpanProcessor,
-  SpanExporter,
 } from "@opentelemetry/sdk-trace-base";
-import { ExportResult } from "@opentelemetry/core";
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 class NextJsSpanProcessor implements SpanProcessor {
   forceFlush(): Promise<void> {
@@ -30,28 +32,24 @@ class NextJsSpanProcessor implements SpanProcessor {
   }
 }
 
-class CustomSpanExporter implements SpanExporter {
-  export(
-    spans: ReadableSpan[],
-    resultCallback: (result: ExportResult) => void
-  ): void {}
-  shutdown(): Promise<void> {
-    return Promise.resolve();
-  }
-  forceFlush?(): Promise<void> {
-    return Promise.resolve();
-  }
-}
-
 export function register() {
-  registerOTel({
-    spanProcessors: [new NextJsSpanProcessor(), "auto"],
-    traceExporter: new CustomSpanExporter(),
-  });
-
-  Sentry.init({
+  const sentryClient = Sentry.init({
     tracesSampleRate: 1.0,
     skipOpenTelemetrySetup: true,
     dsn: SENTRY_DSN,
   });
+
+  const provider = new NodeTracerProvider({
+    sampler: sentryClient ? new SentrySampler(sentryClient) : undefined,
+  });
+
+  provider.addSpanProcessor(new SentrySpanProcessor());
+  provider.addSpanProcessor(new NextJsSpanProcessor());
+
+  provider.register({
+    propagator: new SentryPropagator(),
+    contextManager: new Sentry.SentryContextManager(),
+  });
+
+  Sentry.validateOpenTelemetrySetup();
 }
